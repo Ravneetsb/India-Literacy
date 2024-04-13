d69397d24bb6151a28fc2d8617720ae3bf1ef9e2
Ravneet Bhatia <rsbhatia2@usfca.edu>
2024-04-12 20:14:21 -0700

    alpha release scatter

--- scatter.js
+++ scatter.js
@@ -1,37 +1,39 @@
-const width = 1000;
-const height = 800;
-const marginTop = 50;
-const marginBottom = 50;
-const marginRight = 120;
-const marginLeft = 150;
+const factor = 1.5;
+const ScatterWidth = 1000 / factor;
+const ScatterHeight = 800 / factor;
+const marginTop = 50 / factor;
+const marginBottom = 50 / factor;
+const marginRight = 120 / factor;
+const marginLeft = 150 / factor;
 
 d3.csv("data/density.csv", d => {
     return {
         state: d.State,
-        literacy: d.Literacy,
+        literacy: +d.Literacy,
         population: +d.Population,
         schools: +d.TotalSchools
     };
 }).then((data) => {
     const svg = d3.select("#scatter").append("svg")
-        .attr("height", height)
-        .attr("width", width);
+        .attr("height", ScatterHeight)
+        .attr("width", ScatterWidth);
 
     const x = d3.scaleLinear()
-        .domain(d3.extent(data, d =>d.population))
-        .range([marginLeft, width - marginRight]);
+        .domain(d3.extent(data, d =>d.schools))
+        .range([marginLeft, ScatterWidth - marginRight]);
 
     const y = d3.scaleLinear()
-        .domain(d3.extent(data, d => d.schools))
-        .range([marginBottom, height - marginTop]);
+        .domain(d3.extent(data, d => d.population))
+        .range([ScatterHeight - marginTop, marginBottom]);
+
 
     const colorScale = d3.scaleLinear()
-        .domain(d3.extent(d.literacy))
-        .interpolate(d3.interpolateBlues);
+        .domain(d3.extent(data, d => d.literacy))
+        .range(['#edf8fb','#238b45'])
 
     svg.append("g")
         .attr("class", "xAxis")
-        .attr("transform", `translate(0,${height - marginBottom})`)
+        .attr("transform", `translate(0,${ScatterHeight - marginBottom})`)
         .call(d3.axisBottom(x));
 
     svg.append("g")
@@ -42,10 +44,11 @@ d3.csv("data/density.csv", d => {
     svg.selectAll()
         .data(data)
         .enter().append("circle")
-        .attr("cx", d => x(d.population))
-        .attr("cy", d => y(d.schools))
+        .attr("cx", d => x(d.schools))
+        .attr("cy", d => y(d.population))
         .attr("r", 5)
-        .attr("fill", d => colorScale(d.literacy));
+        .attr("fill", d => colorScale(d.literacy))
+        .style("stroke", "black");
 
     const legend = d3.legendColor()
         .scale(colorScale)
@@ -53,14 +56,14 @@ d3.csv("data/density.csv", d => {
 
     svg.append("g")
         .attr("class", "legend")
-        .attr("transform", `translate(${width - marginRight}, ${height - 200})`)
+        .attr("transform", `translate(${ScatterWidth - marginRight - 100}, ${ScatterHeight / 50})`)
         .call(legend);
 
     svg.append("text")
         .attr("class", "ylabel")
-        .attr("x", -height / 2)
+        .attr("x", -ScatterHeight / 2)
         .attr("y", marginLeft / 2)
-        .text("Culmen Depth")
+        .text("Number of Schools")
         .attr("transform", "rotate(-90)")
         .style("font-size", "20px")
         .style("text-anchor", "middle")
@@ -68,10 +71,10 @@ d3.csv("data/density.csv", d => {
 
     svg.append("text")
         .attr("class", "xlabel")
-        .attr("x", (width) / 2)
-        .attr("y", height-10)
-        .text("Culmen Length")
-        .style("font-size", "20px")
+        .attr("x", (ScatterWidth) / 2)
+        .attr("y", ScatterHeight-10)
+        .text("Population")
+        .style("font-size", "12px")
         .style("text-anchor", "middle")
         .style("font-family", "Arial");
 });

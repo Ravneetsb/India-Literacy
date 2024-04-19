5f2897db70ce2ce7a9f24291fb653d8aac5857bd
Ravneet Bhatia <rsbhatia2@usfca.edu>
2024-04-19 13:22:37 -0700

    alpha release

---
+++ beta-release/map.js
@@ -0,0 +1,97 @@
+var width = 500,
+    height = 500;
+
+var projection = d3.geoMercator();
+
+var path = d3
+    .geoPath()
+    .projection(projection)
+    .pointRadius(2);
+
+var svg = d3
+    .select('#map')
+    .append('svg')
+    .attr('width', width)
+    .attr('height', height);
+
+var g = svg.append('g');
+
+d3.json(
+    'data/india.json',
+).then(function (data) {
+    var boundary = centerZoom(data);
+    var subunits = drawStates(data);
+    // colorStates(subunits);
+    drawBoundary(data, boundary);
+});
+
+function centerZoom(data) {
+    var o = topojson.mesh(
+        data,
+        data.objects.polygons,
+        function (a, b) {
+            return a === b;
+        },
+    );
+
+    projection.scale(1).translate([0, 0]);
+
+    var b = path.bounds(o),
+        s =
+            1 /
+            Math.max(
+                (b[1][0] - b[0][0]) / width,
+                (b[1][1] - b[0][1]) / height,
+            ),
+        t = [
+            (width - s * (b[1][0] + b[0][0])) / 2,
+            (height - s * (b[1][1] + b[0][1])) / 2,
+        ];
+
+    var p = projection.scale(s).translate(t);
+
+    return o;
+}
+
+function drawBoundary(data, boundary) {
+    g.append('path')
+        .datum(boundary)
+        .attr('d', path)
+        .attr('class', 'subunit-boundary')
+        .attr('fill', 'none')
+        .attr('stroke', '#3a403d');
+}
+
+
+function drawStates(data) {
+    var colorScale = d3.scaleSequential(d3.interpolateBlues).domain([60, 100]);
+    var subunits = g
+        .selectAll('.subunit')
+        .data(
+            topojson.feature(data, data.objects.polygons)
+                .features,
+        )
+        .enter()
+        .append('path')
+        .attr('class', 'subunit')
+        .attr('d', path)
+        .style('stroke', '#fff')
+        .style('stroke-width', '1px')
+        .style("fill", d => {
+            return d.properties.literacy !== 0? colorScale(d.properties.literacy):"black";
+        });
+    return subunits;
+}
+
+function colorStates(subunits, data) {
+    var colorScale = d3.scaleOrdinal(
+        d3.schemeCategory10,
+    );
+
+    subunits
+        .style('fill', function (d, i) {
+            return colorScale(d.properties.literacy);
+        })
+        .style('opacity', '.6');
+
+}
---
+++ beta-release/scatter.js
@@ -0,0 +1,80 @@
+const factor = 1.5;
+const ScatterWidth = 1000 / factor;
+const ScatterHeight = 800 / factor;
+const marginTop = 50 / factor;
+const marginBottom = 50 / factor;
+const marginRight = 120 / factor;
+const marginLeft = 150 / factor;
+
+d3.csv("data/density.csv", d => {
+    return {
+        state: d.State,
+        literacy: +d.Literacy,
+        population: +d.Population,
+        schools: +d.TotalSchools
+    };
+}).then((data) => {
+    const svg = d3.select("#scatter").append("svg")
+        .attr("height", ScatterHeight)
+        .attr("width", ScatterWidth);
+
+    const x = d3.scaleLinear()
+        .domain(d3.extent(data, d =>d.schools))
+        .range([marginLeft, ScatterWidth - marginRight]);
+
+    const y = d3.scaleLinear()
+        .domain(d3.extent(data, d => d.population))
+        .range([ScatterHeight - marginTop, marginBottom]);
+
+
+    const colorScale = d3.scaleLinear()
+        .domain(d3.extent(data, d => d.literacy))
+        .range(['#edf8fb','#238b45'])
+
+    svg.append("g")
+        .attr("class", "xAxis")
+        .attr("transform", `translate(0,${ScatterHeight - marginBottom})`)
+        .call(d3.axisBottom(x));
+
+    svg.append("g")
+        .attr("class", "yAxis")
+        .attr("transform", `translate(${marginLeft}, 0)`)
+        .call(d3.axisLeft(y));
+
+    svg.selectAll()
+        .data(data)
+        .enter().append("circle")
+        .attr("cx", d => x(d.schools))
+        .attr("cy", d => y(d.population))
+        .attr("r", 5)
+        .attr("fill", d => colorScale(d.literacy))
+        .style("stroke", "black");
+
+    const legend = d3.legendColor()
+        .scale(colorScale)
+        .title("Literacy Level");
+
+    svg.append("g")
+        .attr("class", "legend")
+        .attr("transform", `translate(${ScatterWidth - marginRight - 100}, ${ScatterHeight / 50})`)
+        .call(legend);
+
+    svg.append("text")
+        .attr("class", "ylabel")
+        .attr("x", -ScatterHeight / 2)
+        .attr("y", marginLeft / 2)
+        .text("Number of Schools")
+        .attr("transform", "rotate(-90)")
+        .style("font-size", "20px")
+        .style("text-anchor", "middle")
+        .style("font-family", "Arial");
+
+    svg.append("text")
+        .attr("class", "xlabel")
+        .attr("x", (ScatterWidth) / 2)
+        .attr("y", ScatterHeight)
+        .text("Population")
+        .style("font-size", "12px")
+        .style("text-anchor", "middle")
+        .style("font-family", "Arial");
+});
